b59ce5a197a203ae2b8a21d9c0eb360fd4d47742
efegure <efe__gure@hotmail.com>
2025-05-29 12:36:35 +0300

    feat: build visualizer + note added to vite config, added build visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -12,10 +13,16 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
     tailwindcss(),
+    // !! IMPORTANT NOTE: chunk is big because of mocking library faker.js
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
